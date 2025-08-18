22baf0c56026b5f9af6ff84280cb3848c15c4b78
Kartik Verma <vermakartik667@gmail.com>
2025-08-18 15:25:11 +0530

    react router dom setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react'
 import { Firebasecontext } from './contexts/Firebase'
+import { Route, Routes, Link } from 'react-router-dom'
+import Page1 from './pages/Page1'
 function App() {
   const {name,setname}=useContext(Firebasecontext)
 
@@ -9,6 +11,13 @@ function App() {
     <div>App
       <h1 className=''>hello wassup dude, username is {name ? name:"null" } </h1>
       <button onClick={()=> setname("2nd name ")}>Click me</button>
+
+      <Link to="/page1">Go to page 1</Link>
+
+      <Routes>
+      <Route path='/page1' element={<Page1></Page1>} > </Route>
+
+      </Routes>
 
 
 
--- src/contexts/Firebase.jsx
+++ src/contexts/Firebase.jsx
@@ -1,7 +1,21 @@
 import { createContext, useState } from "react";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 export const Firebasecontext=createContext()
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAsHpMaQ7HTrX1Uh7B9nttH-_4fplW_DJU",
+    authDomain: "newproj-cfebd.firebaseapp.com",
+    projectId: "newproj-cfebd",
+    storageBucket: "newproj-cfebd.firebasestorage.app",
+    messagingSenderId: "124306315954",
+    appId: "1:124306315954:web:4924e777a5976ae19587ab",
+    measurementId: "G-854HBGPTT3"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const FirebaseProvider=({children})=>{
     const [name, setname] = useState("kartik")
